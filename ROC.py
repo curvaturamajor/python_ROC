d80b46d0b18e1728484b913b3f1878515efd9d94
CurvaturaMajor <62203446+curvaturamajor@users.noreply.github.com>
2023-08-12 00:03:00 +0300

    Update ROC.py

--- ROC.py
+++ ROC.py
@@ -2,22 +2,22 @@ from sklearn.metrics import roc_curve,auc
 import matplotlib.pyplot as plt
 
 
-gercek_degerler = [1,1,0,1,0,0,0,1,0,1,1,1]
-tahminler = [0.98,0.43,0.3,0.55,0.35,0.2,0.1,0.60,0.03,0.85,0.35,0.58]
-tahminler_2 = [0.63,0.54,0.4,0.7,0.4,0.66,0.51,0.4,0.3,0.8,0.71,0.4]
+actual_value = [1,1,0,1,0,0,0,1,0,1,1,1]
+predictions = [0.98,0.43,0.3,0.55,0.35,0.2,0.1,0.60,0.03,0.85,0.35,0.58]
+prediction_2 = [0.63,0.54,0.4,0.7,0.4,0.66,0.51,0.4,0.3,0.8,0.71,0.4]
 
-ypo, duyarlilik, esik_degerleri = roc_curve(gercek_degerler,tahminler)
-auc_1 = auc(ypo,duyarlilik)
+false_positive_rate, sensitivity, threshold_values = roc_curve(actual_value,predictions)
+auc_1 = auc(false_positive_rate,sensitivity)
 
-ypo_2, duyarlilik_2, esik_degerleri_2 = roc_curve(gercek_degerler,tahminler_2)
-auc_2 = auc(ypo_2,duyarlilik_2)
+false_positive_rate_2, sensitivity_2, threshold_values_2 = roc_curve(actual_value,predictions_2)
+auc_2 = auc(false_positive_rate_2,sensitivity_2)
 
 
-plt.plot(ypo,duyarlilik,label="Model 1 AUC = %0.3f" %auc_1)
-plt.plot(ypo_2,duyarlilik_2,label="Model 2 AUC = %0.3f" %auc_2)
+plt.plot(false_positive_rate,sensitivity,label="Model 1 AUC = %0.3f" %auc_1)
+plt.plot(false_positive_rate_2,sensitivity_2,label="Model 2 AUC = %0.3f" %auc_2)
 
-plt.xlabel("Yanlış Pozitif Oranı")
-plt.ylabel("Duyarlılık")
+plt.xlabel("False Positive Rate")
+plt.ylabel("Sensitivity")
 
 plt.legend(loc='lower right')
 
